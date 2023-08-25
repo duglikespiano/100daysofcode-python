a93e64fe03c9361ef8f6ab9e7e29514b175b50e6
SUHO PARK <duglikespiano@gmail.com>
2023-08-26 01:38:22 +0900

    DAY1 - STRING, VARIABLE

---
+++ 1_band_name_generator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator')
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of the your pet?\n")
+
+print("Then the band name would be\n" + city_name + "'s " + pet_name)
